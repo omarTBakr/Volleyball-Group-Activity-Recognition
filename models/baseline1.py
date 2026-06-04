ad9c20f199e674e99d9e2e9022ffa13c7c422d9b
omarTBakr <bakro0298@gmail.com>
2026-06-04 17:45:15 +0300

    chaning the model to resent101

--- models/baseline1.py
+++ models/baseline1.py
@@ -55,7 +55,8 @@ class Model(nn.Module):
         super().__init__()
 
         self.num_classes = num_classes
-        self.backbone = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        # self.backbone = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        self.backbone = models.resnet101(weights=models.ResNet101_Weights.DEFAULT)
         self.backbone.fc = nn.Linear(self.backbone.fc.in_features, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
